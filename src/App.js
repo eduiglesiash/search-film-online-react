1c414485fbaad046f1c544d2f8026c5c8eb442eb
Edu Iglesias <eiglesiash@gmail.com>
2018-05-18 23:20:29 +0200

    Componente Form: Control form complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import React, { Component } from 'react';
-import { Title } from "./components/Title";
+import React, {Component} from 'react';
+import {Title} from "./components/Title";
+import {SearchForm} from "./components/SearchForm";
+import {MoviesList} from './components/MoviesList'
 
 import './App.css';
 import 'bulma/css/bulma.css'
 
 class App extends Component {
+
+  state = {
+    results: [],
+    useSearch: false
+  };
+
+  _handleResults = (results) => {
+    this.setState({results, useSearch: true})
+  };
+
+  _renderResult () {
+    return this.state.results.length === 0
+        ? <p>Sorry! Results Not found</p>
+        : <MoviesList movies={this.state.results} />
+  }
+
   render() {
     return (
-      <div className="App">
-        <Title>Search Movies</Title>
-      </div>
+        <div className="App">
+          <Title>Search Movies</Title>
+          <div className={'searchForm-wrapper'}>
+            <SearchForm onResults={this._handleResults}/>
+          </div>
+          {this.state.useSearch
+              ? this._renderResult()
+              : <small>Use the form to search a movie</small>}
+        </div>
     );
   }
 }
--- src/components/Title.js
+++ src/components/Title.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Title = ({children}) => (
     <h1 className="title"> {children} </h1>
-)
+);
 
 
 // <Title title="Search Movies"/>
