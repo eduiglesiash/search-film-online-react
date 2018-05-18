6859b0d1f6c3ebd3996b09b326ee68ff02132a65
Edu Iglesias <eiglesiash@gmail.com>
2018-05-18 22:54:42 +0200

    Update new files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'bulma/css/bulma.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Search Movies</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
